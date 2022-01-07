a38707ffad18036d0b233e5ffc4efb9b4e05af53
jiangwenju <jwj13075323451@dareway.com.cn>
2022-01-07 11:12:55 +0800

    Initial commit

---
+++ app/src/main/java/com/wen/jetpackapp/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.wen.jetpackapp.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
